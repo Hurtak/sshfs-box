003634fafb8dae4882c539893286c8bc6c287d15
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2016-12-12 22:55:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const execa = require('execa')
+const inquirer = require('inquirer')
+
+const config = fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8')
+const {urls, folder} = JSON.parse(config)
+
+const mounted = execa.shellSync('mount').stdout.split('\n')
+
+function isMounted (remote, local) {
+  return mounted.some(mount => mount.startsWith(remote + ' on ' + local))
+}
+
+const data = urls.map(remote => {
+  const local = path.join(folder, remote).replace(/[:].*?$/, '') // user@host:dir -> user@host
+  const isChecked = isMounted(remote, local)
+
+  return {
+    name: `${remote} ↔ ${local}`,
+    checked: isChecked,
+    remote: remote,
+    local: local
+  }
+})
+
+inquirer.prompt([
+  {
+    type: 'checkbox',
+    message: 'SSHFS mount/unmount dirs',
+    name: 'urls',
+    choices: data
+  }
+]).then((answers) => {
+  // mount
+  answers.urls
+    .map(url => data.find(choice => choice.name === url))
+    .forEach(data => {
+      execa.sync('mkdir', ['-p', data.local])
+      const {stderr} = execa.sync('sshfs', [data.remote, data.local])
+      if (stderr) {
+        console.error(stderr)
+        process.exit(1)
+      }
+      console.log(`[x] mounted   ${data.remote}`)
+    })
+
+  // unmount rest
+  data
+    .filter(choice => answers.urls.includes(choice.name) === false)
+    .filter(choice => isMounted(choice.remote, choice.local))
+    .forEach(data => {
+      const res = execa.sync('fusermount', ['-u', data.local])
+      if (res.stderr) {
+        console.error(res.stderr)
+        process.exit(1)
+      }
+      execa.sync('rm', ['-r', data.local])
+      console.log(`[ ] unmounted ${data.remote}`)
+    })
+})
